8c3239f2f67734c4dc835a38e018981f44f68111
Anish Dhamodaran <anishd19@gmail.com>
2017-08-26 00:32:24 +0530

    adds dummyEntries to Page Component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,44 @@ import Page from './Page';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(Date.now()).toLocaleDateString()
+    }
+  }
+  componentWillMount() {
+    this.dummyEntries = [{
+      date : this.state.date,
+      particulars : "Cash Deposit Self",
+      chequeNumber : 12345,
+      debit : 15000,
+      credit : null,
+      balance : 50000
+    },
+    {
+      date : this.state.date,
+      particulars : "Cash Deposit Self",
+      chequeNumber : 12345,
+      debit : 15000,
+      credit : null,
+      balance : 50000
+    },
+    {
+      date : this.state.date,
+      particulars : "Cash Deposit Self",
+      chequeNumber : 12345,
+      debit : 15000,
+      credit : null,
+      balance : 50000
+    }];
+  }
   render() {
     return (
       <div className="App">
-        <Page />
+        <Page
+          entries = {this.dummyEntries}
+        />
       </div>
     );
   }
